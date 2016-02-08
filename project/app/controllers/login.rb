01366a519cceb40be24482008e6abf4029e0176d
Augustin <tataru.augustin.gh@gmail.com>
2016-02-08 16:53:48 +0200

    Login functionality complete

--- project/app/controllers/login.rb
+++ project/app/controllers/login.rb
@@ -1,8 +1,7 @@
 Narms::App.controllers :login do
   get :index do
-    "TODO: redirect if logged in already."
     if current_user
-      render "/worker/profile"
+      redirect nil
     else
       render "/login/login"
     end
@@ -11,18 +10,19 @@ Narms::App.controllers :login do
   post :index do
     user = User.authenticate(params[:email], params[:password])
     if user
-      set_current_user = user
-      "TODO: redirect instead of just rendering."
-      render "/worker/profile"
+      set_current_user user
+      redirect nil
     else
       params[:email] = h(params[:email])
       flash.now[:error] = {'email': 'Invalid email and password combination.'}
-      render "/login/login"
+      redirect url :login, :login
     end
   end
 
   delete :index do
-    sign_out
-    redirect url(:login, :index)
+    if current_user
+      sign_out
+      redirect url :login, :index
+    end
   end
 end
