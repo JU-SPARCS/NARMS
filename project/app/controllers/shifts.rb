c7a6fbca46652e9f3d7e794fd60c5c8a6be5662f
Augustin <tataru.augustin.gh@gmail.com>
2016-02-12 00:05:33 +0200

    Fixed some broken links; Small changes in style; Comments for methods in controller.

--- project/app/controllers/shifts.rb
+++ project/app/controllers/shifts.rb
@@ -1,10 +1,25 @@
 Narms::App.controllers :shifts do
 
+  # CURRENT: renders a view that displays a list of all existing shifts.
+  # HOW SHOULD BE: if the role of the current User is SHIFT_MANAGER or ATCO,
+  # show a list of shifts to which User's worker profiles are linked
   get :index do
     @shifts = Shift.all
     render '/shifts/select_shift'
   end
 
+  # Return a list of Shifts linked to the worker profile with id = :worker_profile
+  get :index, :parent => :workers  do
+    worker = WorkerProfile.find_by_id params[:worker_id]
+    if worker
+      @shifts = worker.shifts
+      render '/shifts/select_shift'
+    else
+      halt 404
+    end
+  end
+
+  # Render information about a Shift with id = :id
   get :index, :with => :id do
     shift = Shift.find_by_id params[:id]
     if shift
--- project/app/helpers/navbar_helper.rb
+++ project/app/helpers/navbar_helper.rb
@@ -4,6 +4,9 @@ module Narms
   class App
     module LogEventsHelper
 
+      # Depending on the role of the User, the navigation bar will look differently.
+      # This method returns the tabs that a User can see, in the format of a map with (key,value) pairs
+      # where key is the tab name, and value is the tab link.
       def visible_tabs
         if current_user
           return {'Home' => '/base', 'Shifts' => '/shifts', 'My Schedules' => '/worker_schedules', 'My Log Events' => '/log_events'}
