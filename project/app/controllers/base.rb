af00c5bb9500401e51d4f18dc78472cac8e6bb5f
Jeremy Harrault <jeremy@linux-0ki3.suse>
2016-02-18 15:03:10 +0100

    fix copy paste error

--- project/app/controllers/base.rb
+++ project/app/controllers/base.rb
@@ -2,13 +2,9 @@
 # Depending on the role of the user the content should be different.
 Narms::App.controllers :base do
 
-  # CURRENT: Because at the moment User is not linked to WorkerProfile, the method sends
-  # ALL existing worker_profiles to the view.
-  #
-  # HOW IT SHOULD BE: The method should return a page where the worker profiles of the current user are show,
-  # if he has role of SHIFT_MANAGER or ATCO
   get :index do
-    @worker_profiles = WorkerProfile.all
+  	# Retrieve the worker profile of the current user
+    @worker_profiles = WorkerProfile.where(user_id: current_user.id)
     render '/base/list'
   end
 
