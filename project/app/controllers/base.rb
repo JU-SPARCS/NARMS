00798352ccb56d8eb1c99dfae230d9227e081d4d
Jean-Guillaume Buret <jg.buret@gmail.com>
2016-02-26 11:51:52 +0100

    Rooted the application on /base controller

--- project/app/controllers/base.rb
+++ project/app/controllers/base.rb
@@ -1,6 +1,6 @@
 # Controller for viewing the home page(base) for a User.
 # Depending on the role of the user the content should be different.
-Narms::App.controllers :base do
+Narms::App.controllers :base, map: '/' do
 
   get :index do
   	date = Date.today
