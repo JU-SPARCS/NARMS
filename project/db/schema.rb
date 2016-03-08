b8f489f08fdf209835f6bfb5e46e58e3f2891c4b
jeyjey78 <jeremy.gros@epitech.eu>
2016-03-08 19:32:23 +0100

    still merge

--- project/db/schema.rb
+++ project/db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 17) do
-=======
-ActiveRecord::Schema.define(version: 19) do
->>>>>>> 312e42a1fecf2370c2a65127efd302da32673832
+ActiveRecord::Schema.define(version: 23) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name",             limit: 255
@@ -67,8 +63,7 @@ ActiveRecord::Schema.define(version: 19) do
     t.string   "prepare_shift_schedule",             limit: 255, default: "nobody"
     t.string   "view_atco_safaps_shift_inputs",      limit: 255, default: "nobody"
     t.string   "view_atco_safaps_shift_assessments", limit: 255, default: "nobody"
-    t.string   "view_atco_worker_schedule",          limit: 255, default: "nobody"
-    t.string   "view_atco_worker_log_event",         limit: 255, default: "nobody"
+    t.string   "manage_atco_safaps_categories",      limit: 255, default: "nobody"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "role_id",                            limit: 4
@@ -87,6 +82,24 @@ ActiveRecord::Schema.define(version: 19) do
   end
 
   add_index "roles", ["name"], name: "index_roles_on_name", using: :btree
+
+  create_table "safaps_profiles", force: :cascade do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "safaps_requests", force: :cascade do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "safaps_responses", force: :cascade do |t|
+    t.integer  "id_evaluation",     limit: 4
+    t.integer  "stress_indicator",  limit: 4
+    t.integer  "fatigue_indicator", limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "schedule_slots_workers", force: :cascade do |t|
     t.datetime "begin"
