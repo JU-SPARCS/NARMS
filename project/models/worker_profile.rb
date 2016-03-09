650237124fd334140a3648d5865608aec73e251c
Jean-Guillaume Buret <jg.buret@gmail.com>
2016-03-09 09:00:01 +0100

    Login page can create user by providing a worker_profile public id

--- project/models/worker_profile.rb
+++ project/models/worker_profile.rb
@@ -18,13 +18,15 @@ class WorkerProfile < ActiveRecord::Base
   include PublicIdentifier
 
   # Associations
-  belongs_to :user, required: true
+  belongs_to :user
   belongs_to :facility, required: true
   has_and_belongs_to_many :shifts, dependent: :destroy
   has_many :schedules, class_name: Schedules::Worker, dependent: :destroy
   has_many :log_events, class_name: Events::Log, dependent: :destroy
 
   # Validations
+  validates_presence_of :user, if: ->() { !new_record? }
+
   # Callbacks
 
   def get_schedules_over_period(start_at, end_at)
