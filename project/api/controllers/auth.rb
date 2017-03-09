cb6905082354a9c2064abf7a1d7c0b011bac97ea
administrator <administrator@SAFAPS-Test.rawlab.dnlab.se>
2017-03-09 17:00:09 +0100

    Detailed info for worker profiles

--- project/api/controllers/auth.rb
+++ project/api/controllers/auth.rb
@@ -2,28 +2,24 @@ Narms::Api.controllers :cals, map: 'cals/auth/' do
 
   post :login do
     user_json = JSON.parse(request.body.read)
-    puts user_json
-
     user = User.authenticate(user_json["email"], user_json["password"])
-    if (user != nil)
-	wk_profiles_array = Array.new		
 
-	wk_profiles = user.worker_profiles
-        wk_profiles.each do |wk_profile|
+    if (user != nil)
+	worker_profiles_list = Array.new		
+        user.worker_profiles.each do |wk_profile|
 		facility = Facility.find(wk_profile.facility_id)
 		wk_profile_formatted = {
-			:worker_profile_pub_id => wk_profile.pub_id,
-			:facility_pub_id => facility.pub_id	
+			:worker_profile => wk_profile,
+			:facility => facility	
 		 }
-		wk_profiles_array.push wk_profile_formatted
+		worker_profiles_list.push wk_profile_formatted
         end
 
-
 	content_type :json
- 	{:success => "True", :worker_profiles => wk_profiles_array}.to_json()
+ 	{:success => "True", :worker_profiles => worker_profiles_list}.to_json
     else
 	content_type :json
-    	{:success => "False"}.to_json()
+    	{:success => "False"}.to_json
     end
   end
 end
